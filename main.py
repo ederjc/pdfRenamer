d3807c871bcf53736b5800e187d933ac257f4706
David A. Thompson <thompdump@gmail.com>
2022-11-07 12:43:07 -0800

    Adds support for defining input files via command-line arguments
    - support specifying the directory containing input files
    
    - support specifying the file pattern for input files

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+import argparse
+import os
+import sys
+
 import pdfRenamer
 
-gui = pdfRenamer.Gui()
-gui.scanFiles("scan-*.pdf")
-gui.showAndRename()
-gui.loop()
+
+def main():
+    """Handle command-line invocation."""
+    parser = argparse.ArgumentParser(description="This is pdfRenamer")
+    parser.add_argument("-m",
+                        help="match filenames against pattern",
+                        action="store",
+                        dest="file_pattern",
+                        type=str)
+    parser.add_argument("input_files_dir", help="directory containing one or more PDF files",
+                        nargs=1, # anticipate supporting input from multiple directories
+                        type=str)
+    args = parser.parse_args()
+    file_pattern = args.file_pattern
+    input_files_dir = args.input_files_dir[0]
+    os.chdir(input_files_dir)
+    try:
+        gui = pdfRenamer.Gui()
+        gui.scanFiles(file_pattern)
+        gui.showAndRename()
+        gui.loop()
+    except Exception as e:
+        lg.error("FIXME")
+        lg.error(sys.exc_info()[0])
+        raise
+
+
+if __name__ == "__main__":
+    main()
